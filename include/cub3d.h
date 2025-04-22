0d94ced95edb6878cc123c8df42c61f179cb9e49
Yves Gille <ygille@student.42lyon.fr>
2025-04-22 17:14:10 +0200

    Error with args

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: ygille <ygille@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 21:38:21 by ygille            #+#    #+#             */
-/*   Updated: 2025/03/21 18:14:16 by ygille           ###   ########.fr       */
+/*   Updated: 2025/04/22 17:13:53 by ygille           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,8 @@
 //	main.c
 
 //	error.c
-void	error(char *error);
-void	display_error(char *error);
+void	error(char *error, char *arg);
+void	display_error(char *error, char *arg);
 
 //	exit.c
 void	exit_set_ctx(t_context *ctx);
@@ -38,5 +38,6 @@ void	free_exit(t_context *ctx);
 
 //	init.c
 void	init_ctx(t_context *ctx);
+void	init_textures(t_context *ctx);
 
 #endif
--- src/error.c
+++ src/error.c
@@ -6,19 +6,19 @@
 /*   By: ygille <ygille@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 22:17:32 by ygille            #+#    #+#             */
-/*   Updated: 2025/03/19 22:42:41 by ygille           ###   ########.fr       */
+/*   Updated: 2025/04/22 17:13:45 by ygille           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void	error(char *error)
+void	error(char *error, char *arg)
 {
-	display_error(error);
+	display_error(error, arg);
 	exit_call();
 }
 
-void	display_error(char *error)
+void	display_error(char *error, char *arg)
 {
-	printf("Error : %s\n", error);
+	printf("Error\n %s %s", error, arg);
 }
