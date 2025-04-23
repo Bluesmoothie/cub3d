876bdd640dac16119a5b96ca6a61221f9796ca66
Yves Gille <ygille@student.42lyon.fr>
2025-04-23 14:17:31 +0200

    Best order

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sithomas <sithomas@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: ygille <ygille@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 21:38:48 by ygille            #+#    #+#             */
-/*   Updated: 2025/04/23 14:10:10 by sithomas         ###   ########.fr       */
+/*   Updated: 2025/04/23 14:17:13 by ygille           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ int	main(int argc, char **argv)
 	t_context	ctx;
 
 	init_ctx(&ctx);
-	init_video(&ctx.mlx);
 	parse(argc, argv, &ctx);
-	// init_textures(&ctx);
+	init_video(&ctx.mlx);
+	init_textures(&ctx);
 	init_hooks_loops(&ctx);
 	exit_call();
 	return (EXIT_SUCCESS);
